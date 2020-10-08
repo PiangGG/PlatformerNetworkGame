0b386491a33b811f63281041767d2001b9334a14
piang <974805475@qq.com>
2020-10-08 18:42:08 +0800

    Section 3 (bug_1)

--- PlatformerGame/Source/PlatformerGame/PlatformerGameInstance.h
+++ PlatformerGame/Source/PlatformerGame/PlatformerGameInstance.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "OnlineSessionSettings.h"
+#include "Interfaces/OnlineSessionInterface.h"
 #include "PlatformerGameInstance.generated.h"
 
 /**
@@ -31,6 +33,100 @@ enum class EInputMode : uint8 {
     EUIAndGame			UMETA(DisplayName = "UI And Game"),
     EGameOnly			UMETA(DisplayName = "Game Only"),
 };
+
+/* Type for providing special settings for the hosted server */
+USTRUCT(BlueprintType)
+struct FBlueprintSessionSetting {
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, Category = "Session Management")
+		FString key;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Session Management")
+		FString value;
+};
+
+/* Type for holding our SearchResult and exposing the needed properties to blueprints*/
+USTRUCT(BlueprintType)
+struct FBlueprintSearchResult {
+	GENERATED_BODY()
+
+	//Our search result. this type is not blueprint accessible
+	FOnlineSessionSearchResult result;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Session Management")
+		FString ServerName;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Session Management")
+		bool bIsInProgress;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Session Management")
+		FString MapName;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Session Management")
+		int PingInMs;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Session Management")
+		int CurrentPlayers;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Session Management")
+		int MaxPlayers;
+
+	//Constructor for empty Search Result
+	FBlueprintSearchResult() {
+		ServerName = FString("No Server Info");
+		PingInMs = -1;
+		bIsInProgress= false ;
+		MapName = FString("No Map Info");
+		CurrentPlayers = 0;
+		MaxPlayers = 0;
+	}
+
+	//Constructor when provided a search result
+	FBlueprintSearchResult(FOnlineSessionSearchResult theResult) 
+	{
+		//keep the result
+		result = theResult;
+
+		//retrieve special settings from the search result
+		ServerName = GetSpecialSettingString(FString("ServerName"));
+		MapName = GetSpecialSettingString(FString("MAPNAME"));
+		FString InProgressString = GetSpecialSettingString(FString("InProgress"));
+
+		if (InProgressString == FString("true")) {
+			bIsInProgress = true;
+		}
+		else {
+			bIsInProgress = false;
+		}
+
+		//get some built in setting data
+		MaxPlayers = result.Session.SessionSettings.NumPublicConnections;
+		CurrentPlayers = MaxPlayers - result.Session.NumOpenPublicConnections;
+		PingInMs = result.PingInMs;
+	}
+
+	//function for getting special setting data from our result
+	FString GetSpecialSettingString(FString key) {
+		//retrieve the session settings
+		FOnlineSessionSettings settings = result.Session.SessionSettings;
+
+		//check to see if the key exists
+		if (settings.Settings.Contains(FName(*key))) {
+			FString value;
+
+			//retrieve the value from the settings
+			settings.Settings[FName(*key)].Data.GetValue(value);
+			return value;
+		}
+
+		//if it doesnt contain that setting
+		return FString("NO DATA AT THAT KEY");
+	}
+};
+/**
+ * 
+ */
 UCLASS()
 class PLATFORMERGAME_API UPlatformerGameInstance : public UGameInstance
 {
@@ -39,6 +135,7 @@ class PLATFORMERGAME_API UPlatformerGameInstance : public UGameInstance
 	//Constructor
 	UPlatformerGameInstance(const FObjectInitializer& ObjectInitializer);
 
+	virtual void Init() override;
 	/* Widget references */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "State Manager")
 	TSubclassOf<class UUserWidget> cMainMenu;
@@ -70,7 +167,124 @@ class PLATFORMERGAME_API UPlatformerGameInstance : public UGameInstance
 	//are we displaying the mouse cursor
 	UPROPERTY(BlueprintReadOnly, Category = "Platformer Game Instance")
 	bool bIsShowingMouseCursor;
-	
+
+	/* Online Session Interface */
+
+	//Shared pointer for holding session settings
+	TSharedPtr<class FOnlineSessionSettings> SessionSettings;
+
+	/* SESSION CREATION */
+	//function for hosting a session from blueprints
+	UFUNCTION(BlueprintCallable, Category = "Session Management")
+	void HostGame(bool bIsLAN, int32 MaxNumPlayers, TArray<FBlueprintSessionSetting> sessionSettings);
+
+	//c++ function for hosting a session
+	bool HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLAN, int32 MaxNumPlayers, TMap<FString, FOnlineSessionSetting> SettingsMap);
+
+	//delegate function which will be called when session is created
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+
+	//delegate to be called when session is created
+	FOnCreateSessionCompleteDelegate OnCreateSessionCompleteDelegate;
+
+	//delegate handle retained after registering delegate
+	FDelegateHandle OnCreateSessionCompleteDelegateHandle;
+
+	/* STARTING A SESSION */
+
+	//delegate function which will be called when session is started
+	void OnStartOnlineGameComplete(FName SessionName, bool bWasSuccessful);
+
+	//delegate to be called when session is started
+	FOnStartSessionCompleteDelegate OnStartSessionCompleteDelegate;
+
+	//delegate handle retained after registering delegate
+	FDelegateHandle OnStartSessionCompleteDelegateHandle;
+
+	/* FINDING SESSIONS */
+
+	//Array for holding our search results
+	UPROPERTY(BlueprintReadOnly, Category = "Session Management")
+	TArray<FBlueprintSearchResult> searchResults;
+
+	//booleans for keeping track of search status
+	UPROPERTY(BlueprintReadOnly, Category = "Session Management")
+		bool bHasFinishedSearchingForGames;
+	UPROPERTY(BlueprintReadOnly, Category = "Session Management")
+		bool bSearchingForGames;
+
+	//shared pointer to our c++ native search results
+	TSharedPtr<class FOnlineSessionSearch> SessionSearch;
+
+	//blueprint function for finding games
+	UFUNCTION(BlueprintCallable, Category = "Session Management")
+	void FindGames(bool bIsLAN);
+
+	//c++ function for finding sessions
+	void FindSessions(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLAN);
+
+	//delegate function called when FindSessions completes
+	void OnFindSessionsComplete(bool bWasSuccessful);
+
+	//Delegate for OnFindSessionsComplete
+	FOnFindSessionsCompleteDelegate OnFindSessionsCompleteDelegate;
+
+	//delegate handle for OnFindSessionsComplete
+	FDelegateHandle OnFindSessionsCompleteDelegateHandle;
+
+
+	/* JOIN SESSIONS */
+	//Blueprint function for joining a session
+	UFUNCTION(BlueprintCallable, Category = "Session Management")
+	void JoinGame(FBlueprintSearchResult result);
+
+	//c++ function for joining the session
+	bool JoinSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, const FOnlineSessionSearchResult& SearchResult);
+
+	//delegate function which will be called when a session has been joined
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+	//delegate for binding our function
+	FOnJoinSessionCompleteDelegate OnJoinSessionCompleteDelegate;
+
+	//delegate handle for OnJoinSessionCompleteDelegate
+	FDelegateHandle OnJoinSessionCompleteDelegateHandle;
+
+	/* UPDATING SESSION */
+	//function for getting the current value of a special setting for the active session
+	UFUNCTION(BlueprintCallable, Category = "Session Management")
+	FString GetSessionSpecialSettingString(FString key);
+
+	//function for creating a new or updating an existing special setting on the active session
+	//host only
+	UFUNCTION(BlueprintCallable, Category = "Session Management")
+	void SetOrUpdateSessionSpecialSettingString(FBlueprintSessionSetting newSetting);
+
+	//delegate function which will be called after update session completes
+	void OnUpdateSessionComplete(FName SessionName, bool bWasSuccessful);
+
+	//delegate which will be called after updating a session
+	FOnUpdateSessionCompleteDelegate OnUpdateSessionCompleteDelegate;
+
+	//delegate handle for OnUpdateSessionCompleteDelegate
+	FDelegateHandle OnUpdateSessionCompleteDelegateHandle;
+
+	/* DESTROYING A SESSION / LEAVING GAME */
+	//Blueprint function for leaving game
+	UFUNCTION(BlueprintCallable, Category = "Session Management")
+	void LeaveGame();
+
+	//delegate function which will be called after leaving session
+	void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+
+	//delegate which will be called after destroying a session
+	FOnDestroySessionCompleteDelegate OnDestroySessionCompleteDelegate;
+
+	//delegate handle for OnDestroySessionCompleteDelegate
+	FDelegateHandle OnDestroySessionCompleteDelegateHandle;
+
+	/* HANDLE NETWORK ERRORS */
+	void HandleNetworkError(UWorld *World, UNetDriver *NetDriver, ENetworkFailure::Type FailureType, const FString & ErrorString);
 
 	private:
 	//currently displayed widget
